72f550bd6b5def6b76acaef25c35710beff434e7
simonedesocio-arch <simone.desocio@gmail.com>
2026-05-22 09:54:59 +0200

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,3 @@
+import http.server, os
+os.chdir("/Users/simonedesocio/Downloads/think or click - claude code")
+http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, port=8743, bind="127.0.0.1")
